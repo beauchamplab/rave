d05a4344e513bbfc52cfd997a0272a5fe223a42b
dipterix <dipterix.wang@gmail.com>
2018-07-12 22:35:49 -0500

    Changed default onattach scheme

--- R/core_default_setups.R
+++ R/core_default_setups.R
@@ -1,120 +1,123 @@
 # functions to create module,data,rawdata dirs
 
 #' @export
-arrange_data_dir <- function(is_dev = FALSE){
-  data_dir = rave::rave_options('data_dir')
-  raw_dir = rave::rave_options('raw_data_dir')
-  is_changed = FALSE
-
-  if(!dir.exists(data_dir) || is_dev){
+arrange_data_dir <- function(first_time = F, reset = F){
+  if(first_time || reset){
     data_dir = '~/rave_data/data_dir'
-    dir.create(data_dir, showWarnings = F, recursive = T)
+    raw_dir = '~/rave_data/raw_dir'
 
-    # move example data to it
-    message("--------------------")
-    message("Original data directory NOT found! Trying to generate default data repository")
-    message("Original data repository - [", rave::rave_options('data_dir'), '] (invalid/not exist)')
-    message("New data repository - [", data_dir, ']')
+    dir.create(data_dir, showWarnings = F, recursive = T)
+    dir.create(raw_dir, showWarnings = F, recursive = T)
 
     # Russian rocket: RE-DO whenever fails
     file.copy(system.file('data/data_dir', package = 'rave'), '~/rave_data/', recursive = T, overwrite = T)
-
-    is_changed = T
-  }
-  rave_options(data_dir = tools::file_path_as_absolute(data_dir))
-
-
-  if(!dir.exists(raw_dir) || is_dev){
-    raw_dir = '~/rave_data/raw_dir'
-    dir.create(raw_dir, showWarnings = F, recursive = T)
-
-    # move example data to it
-    message("--------------------")
-    message("Original raw-data directory NOT found! Trying to generate default raw-data repository")
-    message("Original raw-data repository - [", rave::rave_options('raw_data_dir'), '] (invalid/not exist)')
-    message("New raw-data repository - [", raw_dir, ']')
-
-    # Russian rocket: RE-DO whenever fails
     file.copy(system.file('data/raw_dir', package = 'rave'), '~/rave_data/', recursive = T, overwrite = T)
-
-    is_changed = T
   }
-  rave_options(raw_data_dir = tools::file_path_as_absolute(raw_dir))
 
-  return(is_changed)
+  if(reset){
+    rave::rave_options(
+      data_dir = data_dir,
+      raw_data_dir = raw_dir
+    )
+  }
+
+  data_dir = rave::rave_options('data_dir')
+  raw_dir = rave::rave_options('raw_data_dir')
+
+  if(!dir.exists(data_dir) || !dir.exists(raw_dir)){
+    logger('Cannot find data directory for RAVE. Please make sure that these folder exists', level = 'ERROR')
+    logger(data_dir, level = 'ERROR')
+    logger(raw_dir, level = 'ERROR')
+    logger('Check existence of these folders, or reset default data repository by typing arrange_data_dir(reset = T)', level = 'ERROR')
+    return(F)
+  }else{
+    rave_options(data_dir = tools::file_path_as_absolute(data_dir))
+    rave_options(raw_data_dir = tools::file_path_as_absolute(raw_dir))
+    return(T)
+  }
+
 }
 
 #' @export
 arrange_modules <- function(
-  look_up_file = rave::rave_options('module_lookup_file'),
-  target_dir = NULL,
-  is_new = FALSE
+  first_time = FALSE, reset = F
 ){
-  is_changed = FALSE
-  # check conditions
-  if(is.null(look_up_file) || !file.exists(look_up_file)){
-    is_new = TRUE
-    is_changed = TRUE
-  }
 
-  if(is_new){
-    look_up_file = '~/rave_modules/modules.csv'
-  }
+  look_up_file = rave::rave_options('module_lookup_file')
+  target_dir = rave::rave_options('module_root_dir')
 
-  tryCatch({
-    read.csv(look_up_file, stringsAsFactors = F)
-  }, error = function(e){
-    NULL
-  }) ->
-    old_modules
-  if(is.null(target_dir)){
-    target_dir = dirname(look_up_file)
-  }
-
-  # makesure target_dir exists
   dir.create(target_dir, recursive = T, showWarnings = F)
-
-  # define destination files
-  # write to look_up_file
   new_modules = read.csv(system.file('modules.csv', package = 'rave'), stringsAsFactors = F)
-  new_modules.copy = new_modules
-  if(is.data.frame(old_modules) && nrow(old_modules)){
-    tryCatch({
-      sel = !old_modules$ModuleID %in% new_modules$ModuleID
-      if(sum(sel)){
-        if(!setequal(names(new_modules), names(old_modules))){
-          n_names = setdiff(names(new_modules), names(old_modules))
-          old_modules[, n_names] = NA
-        }
-        new_modules = rbind(new_modules, old_modules[sel, names(new_modules)])
+
+  if(first_time || reset || !dir.exists(target_dir) || !file.exists(look_up_file)){
+    # unlike data dir, we need to update this every time!
+    try({
+      columns = names(new_modules)
+      if(file.exists(look_up_file)){
+        old_modules = read.csv(look_up_file, stringsAsFactors = F)
       }
-      new_modules = new_modules[complete.cases(new_modules[, c("ModuleID","Name","ScriptPath")]), ]
-      new_modules$Active[is.na(new_modules$Active)] = FALSE
-      new_modules[is.na(new_modules)] = ""
-      new_modules$Active = new_modules$Active & sapply(new_modules$ScriptPath, function(p){tryCatch({
-        file.exists(p)
-      }, error = function(e){FALSE})})
-      new_modules
-    }, error = function(e){
-      warning('Failed to migrate old modules to [', look_up_file, ']')
-      return(new_modules.copy)
-    }) ->
-      new_modules
+      new = merge(new_modules, old_modules, by = 'ModuleID', all = T, sort = F, suffixes = c('', '_old'))
+
+      # check ModuleID
+      new = new[!is.na(new$ModuleID),]
+
+      lapply(seq_len(nrow(new)), function(ii){
+        row = new[ii,]
+
+        for(col in columns){
+          if(is.na(row[[col]])){
+            row[[col]] = row[[paste0(col, '_old')]]
+          }
+        }
+        row$Active = row$Active & row$Active_old
+        if(is_invalid(row$ScriptPath, .invalids = c('null', 'na', 'blank')) || !file.exists(row$ScriptPath)){
+          row$Active = F
+        }
+        if(is.na(row$Version) || !is.character(row$Version)){
+          row$Version = '0'
+        }
+        row[, columns]
+      }) ->
+        modules
+
+      new = do.call(rbind, modules)
+
+      lapply(modules, function(m){
+        sel = m$ModuleID == new$ModuleID
+        if(sum(sel) > 1){
+          vers = as.numeric_version(new$Version[sel])
+          m_ver = as.numeric_version(m$Version)
+          if(m_ver != max(vers)){
+            return(NULL)
+          }
+        }
+        return(m)
+      }) ->
+        modules
+
+      modules = rave:::dropNulls(modules)
+
+      modules = do.call(rbind, modules)
+      new_modules = modules[!duplicated(modules[,c('ModuleID', 'Version')]), ]
+
+    }, silent = T)
+
+    safe_write_csv(new_modules, look_up_file)
+
+    # Move modules to module_root_dir
+    # migrate new modules and overwrite
+    new_repo = system.file('modules', package = 'rave')
+    file.copy(new_repo, target_dir, overwrite = T, recursive = T)
+
   }
-  write.csv(new_modules, look_up_file)
-  look_up_file = tools::file_path_as_absolute(look_up_file)
-  rave::rave_options(module_lookup_file = look_up_file, module_root_dir = target_dir)
 
-  message("--------------------")
-  message('Active modules: \n', paste0(' - ', new_modules$Name[new_modules$Active], collapse = '\n'),
-          '\nPlease edit [', look_up_file, ']')
+  rave_options(module_root_dir = tools::file_path_as_absolute(target_dir))
+  rave_options(module_lookup_file = tools::file_path_as_absolute(look_up_file))
 
-  # migrate new modules and overwrite
-  new_repo = system.file('modules', package = 'rave')
-  file.copy(new_repo, target_dir, overwrite = T, recursive = T)
+  logger('\nActive modules: \n', paste0(' - ', new_modules$Name[new_modules$Active], '(', new_modules$ModuleID[new_modules$Active], ')', collapse = '\n'),
+          '\nAccording to [', look_up_file, ']', level = 'INFO')
 
-
-  return(is_changed)
+  return(first_time || reset)
 }
 
 
--- R/core_options.R
+++ R/core_options.R
@@ -2,9 +2,10 @@
 
 opt <- list(
   debug = FALSE,
-  raw_data_dir = '../Researches/rave/adhoc/Dipterix/Data/ECoG/neuralData/originalData/',
-  data_dir = '../rafe/hdf5_data/',
-  module_root_dir = '~/rave_modules',
+  check_updates_onstartup = T,
+  raw_data_dir = '~/rave_data/raw_dir/',
+  data_dir = '~/rave_data/data_dir/',
+  module_root_dir = '~/rave_modules/',
   big_object_size = 500000,
   server_time_zone = 'America/Chicago',
   module_export = './export',
