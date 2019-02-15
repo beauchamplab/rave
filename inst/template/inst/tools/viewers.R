5621211ada312c98b30c57539b073b2e70aeb099
dipterix <zw23@rice.edu>
2019-02-15 15:54:13 -0600

    Vignette

--- inst/template/inst/tools/viewers.R
+++ inst/template/inst/tools/viewers.R
@@ -1,27 +1,27 @@
 to_module <- function(module_id, sidebar_width = 3){
   quos = parse_components(module_id)
-  
+
   tempdir = file.path(tempdir(), 'rave_modules', module_id)
   dir.create(tempdir, showWarnings = F, recursive = T)
   tmpfile = tempfile(pattern = module_id, tmpdir = tempdir)
-  
+
   asis = quos$script_env$asis
   asis %?<-% FALSE
-  
+
   src = sapply(quos$script_env$source, function(f){
-    
+
     if(f != '' && file.exists(f)){
-      
+
       if(!asis){
-        # This file is valid R script, 
+        # This file is valid R script,
         fname = unlist(stringr::str_split(stringr::str_trim(f), '/|\\\\'))
         fname = tail(fname, 1)
-        
+
         fpath = file.path(tempdir, fname)
         file.copy(f, fpath, overwrite = TRUE)
-        
+
         cat2("Copying ", f, ' >> ', fpath)
-        
+
         # if the file ends with .R, source it
         if(stringr::str_detect(fname, pattern = '\\.[Rr]$')){
           quo = rlang::quo(source(!!fname, local = TRUE))
@@ -35,37 +35,37 @@ to_module <- function(module_id, sidebar_width = 3){
           return(expr)
         }
       }
-      
-      
+
+
     }else{
       cat2("Cannot find path to ", f, level = 'ERROR')
     }
     return(NULL)
   })
   names(src) = NULL
-  
-  
+
+
   pkg_name = get_package_name()
-  
-  exec = rlang::quo(rave_execute(!!!get_main_function(module_id))) 
-  
+
+  exec = rlang::quo(rave_execute(!!!get_main_function(module_id)))
+
   funs = sapply(names(quos$output_functions), function(nm){
     f = quos$output_functions[[nm]]
-    
+
     s = paste(deparse(rlang::quo_squash(f)), collapse = '\n')
     s = paste(nm, '<-', s)
     s
   }, simplify = T, USE.NAMES = F)
-  
+
   s = unlist(c(
-    src, 
+    src,
     deparse(rlang::quo_squash(quos$rave_inputs_quo)),
     deparse(rlang::quo_squash(quos$rave_update_quo)),
     deparse(rlang::quo_squash(quos$rave_output_quo)),
     deparse(rlang::quo_squash(exec)),
     funs
   ))
-  
+
   writeLines(s, tmpfile)
   m = rave::ModuleEnvir$new(module_id = module_id, label_name = get_module_label(module_id),
                             script_path = tmpfile, parent_env = loadNamespace(pkg_name))
@@ -73,8 +73,11 @@ to_module <- function(module_id, sidebar_width = 3){
   m
 }
 
-view_layout <- function(module_id, sidebar_width = 5, launch.browser = rstudio_viewer){ 
-  m = to_module(module_id = module_id, sidebar_width = sidebar_width)
-  init_app(m, launch.browser = launch.browser, disable_sidebar = T, simplify_header = T)
-  
+view_layout <- function(module_id, sidebar_width = 5, launch.browser = rstudio_viewer){
+  # Always reload the package to the newest status and preview
+  env = reload_this_package()
+
+  m = env$to_module(module_id = module_id, sidebar_width = sidebar_width)
+  rave::init_app(m, launch.browser = launch.browser, disable_sidebar = T, simplify_header = T)
+
 }
