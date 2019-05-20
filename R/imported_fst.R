1162f1176bb287339790bf352192214f9d37e5e8
dipterix <zw23@rice.edu>
2019-05-20 18:12:56 -0500

    create dirs when write_fst dirame not exists

--- R/imported_fst.R
+++ R/imported_fst.R
@@ -1,4 +1,8 @@
 write_fst <- function(x, path, ...){
   logger('Writing to path: ', path)
+  dir = dirname(path)
+  if(!dir.exists(dir)){
+    dir.create(dir, recursive = TRUE, showWarnings = FALSE)
+  }
   fst::write_fst(x = x, path = path, ...)
 }
