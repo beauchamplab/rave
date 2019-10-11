1b43fd8357ef729746ae1a35ae2cec9820ff1fa9
dipterix <dipterix.wang@gmail.com>
2019-10-11 12:13:52 -0500

    Re-write define_output_3d_viewer for UI consistency

--- inst/template/inst/tools/output_widgets.R
+++ inst/template/inst/tools/output_widgets.R
@@ -1,7 +1,7 @@
 define_output_3d_viewer <- function(
   outputId, title, surfaces = 'pial', multiple_subject = F,
   message = 'Generate 3D Viewer ',
-  height = '500px', width = 12, order = 0, additional_ui = NULL,
+  height = NULL, width = 12, order = 0, additional_ui = NULL,
   hide_btn = FALSE, ...
 ){
 
@@ -12,18 +12,17 @@ define_output_3d_viewer <- function(
   output_fun = paste0(outputId, '_fun')
 
   additional_ui = substitute(additional_ui)
+  
+  
 
 
   quo = rlang::quo({
 
     ...local_env = new.env()
 
-    assign(!!output_call, function(){
+    assign(!!outputId, function(){
       clicked = shiny::isolate(input[[!!output_btn]])
 
-      # Monitor subject change. If changed, then refresh!
-      monitor_subject_change()
-      
       if( !!hide_btn ){
         btn = NULL
       }else{
@@ -35,6 +34,15 @@ define_output_3d_viewer <- function(
         ),
         ' | ')
       }
+      
+      if(is.null(!!height)){
+        client_size = get_client_size()
+        client_height = client_size$available_size[[2]] - 200
+        height = sprintf('%.0fpx', client_height)
+      }else{
+        height = !!height
+      }
+      
 
       htmltools::tagList(
         htmltools::div(
@@ -54,7 +62,7 @@ define_output_3d_viewer <- function(
         ),
         htmltools::div(
           style = 'margin: 0 -10px -10px -10px',
-          threeBrain::threejsBrainOutput(ns(!!outputId), height = !!height)
+          threeBrain::threejsBrainOutput(ns(!!output_call), height = height)
         )
       )
     }, envir = environment())
@@ -63,7 +71,8 @@ define_output_3d_viewer <- function(
       input = getDefaultReactiveInput()
       output = getDefaultReactiveOutput()
       session = getDefaultReactiveDomain()
-
+      .env = environment()
+      .env$local_signal = 0
 
       observeEvent(input[[!!output_new]], {
 
@@ -88,7 +97,7 @@ define_output_3d_viewer <- function(
           if(is.null(rave_id)){ rave_id = '' }
           token = session$userData$token
           if(is.null(token)){ token = '' }
-          globalId = ns(!!outputId)
+          globalId = ns(!!output_call)
 
           query_str = list(
             type = '3dviewer',
@@ -105,64 +114,102 @@ define_output_3d_viewer <- function(
 
       render_func = function(){
         threeBrain::renderBrain({
-          brain = rave::rave_brain2(subject = subject, surfaces = !!surfaces)
           
-          shiny::validate(
-            shiny::need(!is.null(brain), message = 'Cannot find surface/volume files')
-          )
-
-          re = brain
-          
+          # Monitor subject change. If changed, then refresh!
+          if(!monitor_subject_change()){
+            return(NULL)
+          }
           local_signal = input[[!!output_btn]]
+          render_value = length(local_signal) && local_signal > .env$local_signal
+          if( render_value ){
+            .env$local_signal = local_signal
+          }
           
-          # Render function
-          if( length(local_signal) && local_signal > 0 ){
-            f = get0(!!output_fun, envir = ..runtime_env, ifnotfound = function(...){
-              rutabaga::cat2('3D Viewer', !!outputId,  'cannot find function', !!output_fun, level = 'INFO')
-            })
-
-            tryCatch({
-              re = f(brain)
-            }, error = function(e){
-              rave::logger(e, level = 'ERROR')
-            })
-
-          }
-
-          if('htmlwidget' %in% class(re)){
-            # User called $view() with additional params, directly call the widget
-            ...local_env$widget = re
-            re
-          }else if('R6' %in% class(re)){
-            # User just returned brain object
-            ...local_env$widget = re$plot()
-            re$plot(side_display = FALSE)
+          # get render function
+          f = get0(!!output_fun, envir = ..runtime_env, ifnotfound = function(...){
+            rutabaga::cat2('3D Viewer', !!outputId,  'cannot find function', !!output_fun, level = 'INFO')
+          })
+          
+          # get client size
+          client_size = get_client_size()
+          if(!is.null(client_size)){
+            side_width = min(ceiling((client_size$available_size[[2]] - 300) / 3), 300)
           }else{
-            # User returned nothing
-            ...local_env$widget = brain$plot()
-            brain$plot(side_display = FALSE)
+            side_width = 250
           }
+          ...local_env$widget = NULL
+          re = f(render_value, side_width, ...local_env)
+          if(is.null(...local_env$widget)){
+            ...local_env$widget = re
+          }
+          re
+          # 
+          # brain = rave::rave_brain2(subject = subject, surfaces = !!surfaces)
+          # 
+          # shiny::validate(
+          #   shiny::need(!is.null(brain), message = 'Cannot find surface/volume files')
+          # )
+          # 
+          # re = brain
+          # 
+          # 
+          # 
+          # # Render function
+          # if( length(local_signal) && local_signal > .env$local_signal ){
+          #   .env$local_signal = local_signal
+          #   f = get0(!!output_fun, envir = ..runtime_env, ifnotfound = function(...){
+          #     rutabaga::cat2('3D Viewer', !!outputId,  'cannot find function', !!output_fun, level = 'INFO')
+          #   })
+          # 
+          #   tryCatch({
+          #     re = f(brain)
+          #   }, error = function(e){
+          #     rave::logger(e, level = 'ERROR')
+          #   })
+          # 
+          # }else{
+          #   ...local_env$widget = re$plot()
+          #   return(re$plot(side_display = FALSE))
+          # }
+          # 
+          # if('htmlwidget' %in% class(re)){
+          #   # User called $view() with additional params, directly call the widget
+          #   ...local_env$widget = re
+          #   re
+          # }else if('R6' %in% class(re)){
+          #   # User just returned brain object
+          #   ...local_env$widget = re$plot()
+          #   re$plot(side_display = FALSE)
+          # }else{
+          #   # User returned nothing
+          #   ...local_env$widget = brain$plot()
+          #   brain$plot(side_display = FALSE)
+          # }
 
 
         })
       }
 
-      # Register render function
-      output[[!!outputId]] <- render_func()
+      # Because monitor_subject_change needs execenv to be ready
+      eval_when_ready(function(...){
+        # Register render function
+        output[[!!output_call]] <- render_func()
+      })
+      
 
       # Register cross-session function so that other sessions can register the same output widget
       session$userData$cross_session_funcs %?<-% list()
       # ns must be defined, but in get_module(..., local=T) will raise error
       # because we are not in shiny environment
       ns %?<-% function(x){x} 
-      session$userData$cross_session_funcs[[ns(!!outputId)]] = render_func
+      session$userData$cross_session_funcs[[ns(!!output_call)]] = render_func
     })
   })
 
   # generate output
   df = rlang::quo({
     define_output(
-      definition = customizedUI(!!output_call),
+      definition = customizedUI(!!outputId),
       title = !!title,
       width = !!width,
       order = !!order
